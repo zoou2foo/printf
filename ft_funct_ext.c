0454b3856c9773f1e65d7585967a42aca072b8f1
Valerie <zoou2foo@gmail.com>
2022-06-13 15:02:43 -0400

    adding more stuff to my printf.c

---
+++ ft_funct_ext.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_funct_ext.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vjean <vjean@student.42quebec.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 14:21:04 by vjean             #+#    #+#             */
+/*   Updated: 2022/06/13 14:54:53 by vjean            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putchar(char c, int *len)
+{
+	write(1, &c, 1);
+	(*len)++;
+}
+
+void	ft_putnbr(int nb, int *len)
+{
+	if (nb <= -2147483647)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void	ft_putstr(char *str, int *len)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{	
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_puthex(int nb, int *len)
+{
+	
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42quebec.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 10:14:02 by vjean             #+#    #+#             */
-/*   Updated: 2022/06/13 14:19:32 by vjean            ###   ########.fr       */
+/*   Updated: 2022/06/13 15:00:30 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,29 @@
 
 int	ft_printf(const char *format, ...)
 {
+	if (i == c)
+		ft_putchar()
 	
+	if (i == s)
+		ft_putstr()
+	
+	if (i == p)
+		ft_puthex()
+	
+	if (i == d)
+		ft_putnbr()
+	
+	if (i == i)
+		ft_putnbr()
+	if (i == u)
+		ft_putnbr()
+	if (i == x)
+		ft_puthex()
+	
+	if (i == X)
+		ft_puthex()
+	if (i == %)
+		ft_putchar() //to check; not sure...
 
 }
 
@@ -33,6 +55,6 @@ int	main()
 	str2 = NULL;
 	printf("Flag c\n");
 	ft_printf("J'ai %d caractères\n", ft_printf("MIEN: %c\n", c));
-	printf("J'ai %d caractères\n", ft_printf("MIEN: %c\n", c));
+	printf("J'ai %d caractères\n", printf("REAL: %c\n", c));
 	return (0);
 }
