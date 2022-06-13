9f56868a8a34aae2cc8139e3630fda2324b95cea
Valerie <zoou2foo@gmail.com>
2022-06-13 14:20:40 -0400

    created main and a file for other funct

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42quebec.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 10:14:02 by vjean             #+#    #+#             */
-/*   Updated: 2022/06/13 12:54:36 by vjean            ###   ########.fr       */
+/*   Updated: 2022/06/13 14:19:32 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 
 int	ft_printf(const char *format, ...)
 {
+	
 
 }
 
 int	main()
 {
+	int		a;
+	int		b;
+	char	c;
+	char	str[];
+	char	*str2;
+
+	a = -2147483648;
+	b = 10;
+	c = 'c';
+	str[] = "madafakas";
+	str2 = NULL;
+	printf("Flag c\n");
+	ft_printf("J'ai %d caractères\n", ft_printf("MIEN: %c\n", c));
+	printf("J'ai %d caractères\n", ft_printf("MIEN: %c\n", c));
 	return (0);
 }
-//red triangle a line or group of lines  was delete. Blue: a line was modified.
-//green: a line was added
